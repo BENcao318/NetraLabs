3af36c6a2ce037e71595f5908a4a1ea0d69750b1
BENcao318 <labber@DESKTOP-U16DFJJ>
2023-07-05 15:30:13 -0400

    Set up the frontend structure for dashboard

--- client/src/layouts/dashboard.jsx
+++ client/src/layouts/dashboard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Sidenav } from '../widgets/layout/sidenav'
 import routes from '../routes'
+import { DashboardNavbar } from './dashboard-navbar'
+import { Route, Routes } from 'react-router-dom'
 
 export const Dashboard = () => {
   return (
@@ -10,7 +12,18 @@ export const Dashboard = () => {
         brandName={'NetraLabs'}
         routes={routes}
       />
-      <div className="p-4 xl:ml-80"></div>
+      <div className="p-3 xl:ml-80">
+        <DashboardNavbar />
+        <Routes>
+          {routes.map(
+            ({ layout, pages }) =>
+              layout === 'dashboard' &&
+              pages.map(({ path, element }) => (
+                <Route exact path={path} element={element} />
+              ))
+          )}
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ client/src/pages/dashboard/resources.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Resources = () => {
+  return (
+    <div className="flex justify-center p-6 h-screen">
+      <div className="text-bold text-2xl self-center ">Resources</div>
+    </div>
+  )
+}
